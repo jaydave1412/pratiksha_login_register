036baaadff9b8ce6bad1ea5881585de3e5f87c1b
pratikshathakur <pratikshathakur360@gmail.com>
2021-02-03 19:33:13 +0530

    Login-Register Page

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+const app = express();
+const registerRouter=require('./routes/users');
+
+require('./config/passport')(passport);
+
+const db = require('./config/keys').MongoURI;
+
+
+mongoose.connect(db,{ useNewUrlParser: true ,useUnifiedTopology: true})
+  .then(() => console.log('Database Connected'))
+  .catch(err => console.log(err));
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Express body parser
+app.use(express.urlencoded({ extended: false }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/users', registerRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on  ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports={
+    MongoURI:'mongodb://Pratiksha:test@cluster0-shard-00-00.jyqk3.mongodb.net:27017,cluster0-shard-00-01.jyqk3.mongodb.net:27017,cluster0-shard-00-02.jyqk3.mongodb.net:27017/test?ssl=true&replicaSet=atlas-me9y9c-shard-0&authSource=admin&retryWrites=true&w=majority'
+    
+}
